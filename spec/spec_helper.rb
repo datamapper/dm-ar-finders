88aaf252b0a18147dce6ef46e471acf5acfc0625
Alex Coles <alexbcoles@mac.com>
2008-05-13 14:03:34 +0200

    Updated plugins specs to allow running under any adapter
     * Updated specs to allow them to be run under any
       adapter by specifying the ADAPTER environment
       variable. Mirroring changes made in commit # 75f4429
       to dm-aggregates plugin.
     * dm-migrations now uses use sqlite3 memory database
       by default in integration tests (dm-migrations specs
       still need to be refactored further, for clarity).
     * Converted dm-ar-finders to use less brittle auto-migrate!
     * Continued separating out integration and unit specs:
       updating paths, added spec_helpers to dm-ar-finders,
       dm-serializer.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent + 'lib/dm-ar-finders'
+
+def load_driver(name, default_uri)
+  return false if ENV['ADAPTER'] != name.to_s
+
+  lib = "do_#{name}"
+
+  begin
+    gem lib, '=0.9.0'
+    require lib
+    DataMapper.setup(name, ENV["#{name.to_s.upcase}_SPEC_URI"] || default_uri)
+    DataMapper::Repository.adapters[:default] =  DataMapper::Repository.adapters[name]
+
+    FileUtils.touch LOG_PATH
+    DataMapper::Logger.new(LOG_PATH, 0)
+    at_exit { DataMapper.logger.close }
+    true
+  rescue Gem::LoadError => e
+    warn "Could not load #{lib}: #{e}"
+    false
+  end
+end
+
+ENV['ADAPTER'] ||= 'sqlite3'
+LOG_PATH     = Pathname(__FILE__).dirname.expand_path.to_s + '/sql.log'
+HAS_SQLITE3  = load_driver(:sqlite3,  'sqlite3::memory:')
+HAS_MYSQL    = load_driver(:mysql,    'mysql://localhost/dm_core_test')
+HAS_POSTGRES = load_driver(:postgres, 'postgres://postgres@localhost/dm_core_test')
