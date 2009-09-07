f71e0b4692cbf78ed9d829f0ed36d4544a3e38c0
snusnu <gamsnjaga@gmail.com>
2009-09-07 04:59:55 +0200

    [all] Improved rubygems citizenship
    Removed a lot of the complexity in specifying
    require paths and which specs to run, relying
    on defaults and not assuming rubygems as much as
    is practical.
    
    It's still possible to either run 'rake spec' or
    run the 'spec' command on individual files. If
    you want to run specs for individual files, you
    will need to do something to the effect of:
    
      export RUBYOPT=-Ispec
    
    Also, when running from a typical dev setup
    (dm-core and dm-more are in the same folder),
    specs will pick up the local version of dm-core
    instead of the gem version. This also happens
    for dm-more plugin specs that depend on other
    dm-more plugins.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
-require 'pathname'
-require 'rubygems'
-
-gem 'dm-core', '0.10.0'
+# use local dm-core if running from a typical dev checkout.
+lib = File.join('..', '..', 'dm-core', 'lib')
+$LOAD_PATH.unshift(lib) if File.directory?(lib)
 require 'dm-core'
 
-require Pathname(__FILE__).dirname.parent / 'lib' / 'dm-ar-finders'
+# Support running specs with 'rake spec' and 'spec'
+$LOAD_PATH.unshift(File.join('lib'))
+
+require 'dm-ar-finders'
 
 def load_driver(name, default_uri)
   return false if ENV['ADAPTER'] != name.to_s
@@ -12,10 +14,6 @@ def load_driver(name, default_uri)
   begin
     DataMapper.setup(name, ENV["#{name.to_s.upcase}_SPEC_URI"] || default_uri)
     DataMapper::Repository.adapters[:default] =  DataMapper::Repository.adapters[name]
-
-    FileUtils.touch LOG_PATH
-    DataMapper::Logger.new(LOG_PATH, :debug)
-    at_exit { DataMapper.logger.close }
     true
   rescue LoadError => e
     warn "Could not load do_#{name}: #{e}"
@@ -24,7 +22,7 @@ def load_driver(name, default_uri)
 end
 
 ENV['ADAPTER'] ||= 'sqlite3'
-LOG_PATH     = Pathname(__FILE__).dirname / 'sql.log'
+
 HAS_SQLITE3  = load_driver(:sqlite3,  'sqlite3::memory:')
 HAS_MYSQL    = load_driver(:mysql,    'mysql://localhost/dm_core_test')
 HAS_POSTGRES = load_driver(:postgres, 'postgres://postgres@localhost/dm_core_test')
